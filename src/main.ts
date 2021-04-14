ce13a62c1cb5430e4992d19b6f8d3601b700d9f6
thwbm <thwbm@qq.com>
2021-04-14 20:49:02 +0800

    fix：修该element-plus按需引入

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
+import "element-plus/packages/theme-chalk/src/base.scss";
+import { useElementPlus } from "./elementPlus";
 
-createApp(App)
-  .use(router)
-  .use(ElementPlus, { size: "small", zIndex: 3000 })
-  .mount("#app");
+const app = createApp(App);
+useElementPlus(app);
+app.use(router);
+app.mount("#app");
